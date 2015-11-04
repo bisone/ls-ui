1bbb4bab485c21c6c8cf966787f0a22aff58102a
Will <changwei.cn@gmail.com>
2015-11-04 13:23:12 +0800

    release

--- public/index.js
+++ public/index.js
@@ -4,5 +4,5 @@ webpackJsonp([2],{0:/*!*******************!*\
 function(e,t,r){e.exports=r(/*! /home/will/ls-ui/app/index.js */114)},114:/*!**********************!*\
   !*** ./app/index.js ***!
   \**********************/
-function(e,t,r){"use strict";function n(e){return e&&e.__esModule?e:{"default":e}}var o=r(/*! react */9),i=(n(o),r(/*! react-dom */41));n(i)}});
+function(e,t,r){"use strict";function n(e){return e&&e.__esModule?e:{"default":e}}Object.defineProperty(t,"__esModule",{value:!0});var i=r(/*! ./components/jqgrid/JQGrid.jsx */65),o=n(i);t.JQGrid=o["default"]}});
 //# sourceMappingURL=index.js.map
