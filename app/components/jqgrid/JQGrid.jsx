25c43b429edb90486e4bd0633884e29b6df71666
Will <changwei.cn@gmail.com>
2015-11-17 11:30:00 +0800

    可以合并单元格

--- app/components/jqgrid/JQGrid.jsx
+++ app/components/jqgrid/JQGrid.jsx
@@ -20,11 +20,34 @@ export default class JQGrid extends React.Component{
         $(this.refs.eventsgrid).jqGrid(
             $.extend(
                 {
+                    viewrecords: true, // show the current page, data rang and total records on the toolbar
+                    datatype: "local",
+                    height: 'auto',
+                    width: 'auto',
+                    autowidth:true,//自动宽
                     pager: '#' + this.state.pagerID,
                     rowNum: 10
                 },
                 this.props.options,
             ));
+
+        // activate the toolbar searching
+        $(this.refs.eventsgrid).jqGrid('filterToolbar');
+        $(this.refs.eventsgrid).jqGrid('navGrid',"#"+this.state.pagerID, {
+            search: true, // show search button on the toolbar
+            add: false,
+            edit: false,
+            del: false,
+            refresh: true
+        });
+
+        //表头合并
+        if(this.props.groupHeaderOption){
+            $(this.refs.eventsgrid).jqGrid('setGroupHeaders', {
+                useColSpanStyle: true,
+                groupHeaders: this.props.groupHeaderOption
+            });
+        }
     }
     componentWillUpdate(){
         $(this.refs.eventsgrid).GridUnload();
@@ -44,33 +67,3 @@ export default class JQGrid extends React.Component{
         );
     }
 }
-JQGrid.defaultProps={
-    options:{
-        datatype: "local",
-        data: [
-            { id: "1", invdate: "2007-10-01", name: "test", note: "note", amount: "200.00", tax: "10.00", total: "210.00" },
-            { id: "2", invdate: "2007-10-02", name: "test2", note: "note2", amount: "300.00", tax: "20.00", total: "320.00" },
-            { id: "3", invdate: "2007-09-01", name: "test3", note: "note3", amount: "400.00", tax: "30.00", total: "430.00" },
-            { id: "4", invdate: "2007-10-04", name: "test", note: "note", amount: "200.00", tax: "10.00", total: "210.00" },
-            { id: "5", invdate: "2007-10-05", name: "test2", note: "note2", amount: "300.00", tax: "20.00", total: "320.00" },
-            { id: "6", invdate: "2007-09-06", name: "test3", note: "note3", amount: "400.00", tax: "30.00", total: "430.00" },
-            { id: "7", invdate: "2007-10-04", name: "test", note: "note", amount: "200.00", tax: "10.00", total: "210.00" },
-            { id: "8", invdate: "2007-10-03", name: "test2", note: "note2", amount: "300.00", tax: "20.00", total: "320.00" },
-            { id: "9", invdate: "2007-09-01", name: "test3", note: "note3", amount: "400.00", tax: "30.00", total: "430.00" }
-        ],
-        height: 250,
-        width: 780,
-        colModel: [
-            { label: 'Inv No', name: 'id', width: 75, key:true },
-            { label: 'Date', name: 'invdate', width: 90 },
-            { label: 'Client', name: 'name', width: 100 },
-            { label: 'Amount', name: 'amount', width: 80 },
-            { label: 'Tax', name: 'tax', width: 80 },
-            { label: 'Total', name: 'total', width: 80 },
-            { label: 'Notes', name: 'note', width: 150 }
-        ],
-        viewrecords: true, // show the current page, data rang and total records on the toolbar
-        caption: "Load jqGrid through Javascript Array",
-    }
-
-};
